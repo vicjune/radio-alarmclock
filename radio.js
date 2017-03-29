ccbb937ce8e5c44d6159a89fd779feb674404a98
vicjune <victor@ileotech.com>
2017-03-29 18:07:29 +0200

    [radio] add websockets

--- radio.js
+++ radio.js
@@ -2,7 +2,10 @@ let lame = require('lame');
 let icecast = require('icecast');
 let Speaker = require('speaker');
 let loudness = require('loudness');
+let WebSocket = require('ws');
 
+
+// TEMP MOCK
 let url = 'http://chai5she.cdn.dvmr.fr:80/franceinfo-midfi.mp3';
 let alarms = [{
     days: [1, 2, 3, 4, 5],
@@ -12,6 +15,41 @@ let alarms = [{
 let duration = 60;
 let increase = 5;
 
+
+
+// TODO Database
+
+
+
+// Websocket
+let socketServer = new WebSocket.Server({port: 8001, perMessageDeflate: false});
+socketServer.connectionCount = 0;
+socketServer.on('connection', socket => {
+    socketServer.connectionCount++;
+    console.log(
+        'New WebSocket Connection: ',
+        socket.upgradeReq.socket.remoteAddress,
+        socket.upgradeReq.headers['user-agent'],
+        '('+socketServer.connectionCount+' total)'
+    );
+    socket.on('close', (code, message) => {
+        socketServer.connectionCount--;
+        console.log(
+            'Disconnected WebSocket ('+socketServer.connectionCount+' total)'
+        );
+    });
+});
+socketServer.broadcast = function(data) {
+    socketServer.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+
+
+// Radio + clock
 let streamPlaying = false;
 let durationTimeout;
 
@@ -34,7 +72,7 @@ function startClock() {
         let now = new Date();
         let triggerAlarm = false;
         for (let alarm of alarms) {
-            triggerAlarm = triggerAlarm || alarm.days.indexOf(now.getDay()) >= 0 && now.getHours() === alarm.hour && now.getMinutes() === alarm.minute;
+            triggerAlarm = triggerAlarm || alarm.days.indexOf(now.getDay()) >= 0 && now.getHours() === alarm.hour && now.getMinutes() === alarm.minute && now.getSeconds() === 0;
         }
         if (triggerAlarm) {
             if (streamPlaying) {
@@ -44,29 +82,50 @@ function startClock() {
                 startStream(true);
             }
         }
-    }, 60000);
+    }, 1000);
 }
 
 function startStream(incremental) {
-    streamPlaying = true;
-
-    if (incremental) {
-        console.log('Alarm');
-        let volume = 60;
-        let interval = setInterval(() => {
-            volume = volume + (100 - 60) / (increase * 60);
-            if (volume <= 100 && streamPlaying) {
-                loudness.setVolume(Math.floor(volume), err => {});
-            } else {
-                clearInterval(interval);
-            }
-        }, 1000);
-    }
+    toggleStream(true, incremental);
 
     durationTimeout = setTimeout(() => {
+        toggleStream(false);
+    }, duration * 60000);
+}
+
+function toggleStream(on, incremental = false) {
+    socketServer.broadcast({
+        type: 'toggleStream',
+        data: on
+    });
+
+    if (on) {
+        streamPlaying = true;
+
+        if (incremental) {
+            console.log('Alarm');
+            let volume = 60;
+            let interval = setInterval(() => {
+                volume = volume + (100 - 60) / (increase * 60);
+                if (volume <= 100 && streamPlaying) {
+                    setVolume(Math.floor(volume))
+                } else {
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+    } else {
         streamPlaying = false;
         loudness.setVolume(0, err => {});
         console.log('Alarm stopped');
-    }, duration * 60000);
+    }
+}
+
+function setVolume(volume) {
+    socketServer.broadcast({
+        type: 'volume',
+        data: volume
+    });
+    loudness.setVolume(volume, err => {});
 }
 
