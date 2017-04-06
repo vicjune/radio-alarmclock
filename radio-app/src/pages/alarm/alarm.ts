ee3bd71eeac6cb01936e9e9887252981e36572e2
vicjune <victor.jouin@gmail.com>
2017-04-06 23:36:05 +0200

    [app] improve alarm list and create alarm modal

--- radio-app/src/pages/alarm/alarm.ts
+++ radio-app/src/pages/alarm/alarm.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
+import { NavParams, ViewController } from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+import { Alarm } from '../../interfaces/alarm';
 
 @Component({
   selector: 'page-alarm',
   templateUrl: 'alarm.html'
 })
 export class AlarmPage {
+    alarm: Alarm;
+    newAlarm: boolean;
 
-  constructor(public navCtrl: NavController) {
+    constructor(params: NavParams, public viewCtrl: ViewController) {
+        if (params.get('alarm')) {
+            this.alarm = params.get('alarm');
+            this.alarm.enabled = true;
+            this.newAlarm = false;
+        } else {
+            let now = new Date();
+            this.alarm = {
+                id : + now,
+                days: [],
+                hour: now.getHours(),
+                minute: now.getMinutes(),
+                enabled: true,
+                loading: true
+            }
+            this.newAlarm = true;
+        }
+    }
 
-  }
+    save(): void {
+        this.viewCtrl.dismiss(this.alarm);
+    }
+
+    close(): void {
+        this.viewCtrl.dismiss();
+    }
+
+    delete(): void {
+        this.alarm.enabled = false;
+        this.save();
+    }
 
 }
--- radio-app/src/pages/home/home.ts
+++ radio-app/src/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 
 import { Alarm } from '../../interfaces/alarm';
 import { AlarmPage } from '../alarm/alarm';
+import { SettingsPage } from '../settings/settings';
 import { FireService } from '../../services/fire.service';
 
 @Component({
@@ -13,62 +15,83 @@ import { FireService } from '../../services/fire.service';
 export class HomePage {
     alarms: Alarm[] = [];
 
-    constructor(platform: Platform, public navCtrl: NavController, public fireService: FireService) {
+    constructor(platform: Platform, public navCtrl: NavController, public modalCtrl: ModalController, public fireService: FireService) {
         platform.ready().then(() => {
 
             this.fireService.bind('alarm').subscribe(serverAlarm => {
-                let alarmExists = false;
-                for (let alarm of this.alarms) {
-                    if (alarm.id === serverAlarm.id) {
-                        alarm.days = serverAlarm.days;
-                        alarm.hour = serverAlarm.hour;
-                        alarm.minute = serverAlarm.minute;
-                        alarm.enabled = serverAlarm.enabled;
-                        alarm.loading = false;
-                        alarmExists = true;
-                        break;
-                    }
-                }
-                if (!alarmExists) {
-                    this.alarms.push({
-                        id: serverAlarm.id,
-                        days: serverAlarm.days,
-                        hour: serverAlarm.hour,
-                        minute: serverAlarm.minute,
-                        enabled: serverAlarm.enabled,
-                        loading: false
-                    });
-                }
+                this.editAlarm(serverAlarm, false);
             });
 
         });
     }
 
-    onAlarmToggle(event: boolean, id: number) {
-        for (let i = 0; i < this.alarms.length; ++i) {
-            if (this.alarms[i].id === id) {
-                this.fireService.send('alarm', {
-                    id: id,
-                    days: this.alarms[i].days,
-                    hour: this.alarms[i].hour,
-                    minute: this.alarms[i].minute,
-                    enabled: event
-                });
-                this.alarms[i].enabled = event;
-                this.alarms[i].loading = true;
+    editAlarm(newAlarm, loading: boolean): void {
+        let alarmExists = false;
+        for (let alarm of this.alarms) {
+            if (alarm.id === newAlarm.id) {
+                alarm.days = newAlarm.days;
+                alarm.hour = newAlarm.hour;
+                alarm.minute = newAlarm.minute;
+                alarm.enabled = newAlarm.enabled;
+                alarm.loading = loading;
+                alarmExists = true;
                 break;
             }
         }
+        if (!alarmExists) {
+            this.alarms.push({
+                id: newAlarm.id,
+                days: newAlarm.days,
+                hour: newAlarm.hour,
+                minute: newAlarm.minute,
+                enabled: newAlarm.enabled,
+                loading: loading
+            });
+        }
     }
 
-    alarmSelected(alarm: Alarm = null) {
+    onAlarmToggle(event: boolean, alarm: Alarm): void {
+        alarm.enabled = event;
+        this.editAlarm(alarm, true);
+        this.fireService.send('alarm', {
+            id: alarm.id,
+            days: alarm.days,
+            hour: alarm.hour,
+            minute: alarm.minute,
+            enabled: event
+        });
+    }
+
+    alarmSelected(alarm: Alarm = null): void {
+        let alarmModal;
         if (alarm) {
             if (!alarm.loading) {
-                this.navCtrl.push(AlarmPage); // + alarm parametter
+                alarmModal = this.modalCtrl.create(AlarmPage, {alarm: alarm});
             }
         } else {
-            this.navCtrl.push(AlarmPage);
+            alarmModal = this.modalCtrl.create(AlarmPage);
         }
+        alarmModal.onDidDismiss(modalAlarm => {
+            if (modalAlarm) {
+                if (modalAlarm.enabled) {
+                    this.editAlarm(modalAlarm, true);
+                    this.fireService.send('alarm', {
+                        id: modalAlarm.id,
+                        days: modalAlarm.days,
+                        hour: modalAlarm.hour,
+                        minute: modalAlarm.minute,
+                        enabled: modalAlarm.enabled
+                    });
+                } else {
+                    // this.deleteAlarm(modalAlarm);
+                }
+            }
+        });
+        alarmModal.present();
+    }
+
+    goSettings(): void {
+        this.navCtrl.push(SettingsPage);
     }
 
 }
