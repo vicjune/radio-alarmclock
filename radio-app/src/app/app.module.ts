917180d10ada2798ceb4cb4ff31e4b949f42091a
vicjune <victor@ileotech.com>
2017-05-23 11:51:13 +0200

    [app] update to angular 4 and ionic 3

--- radio-app/src/app/app.module.ts
+++ radio-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -39,6 +40,7 @@ import { MinutesHoursPipe } from '../pipes/minutes-hours.pipe';
 		MinutesHoursPipe
 	],
 	imports: [
+		BrowserModule,
 		IonicModule.forRoot(MyApp)
 	],
 	bootstrap: [IonicApp],
