2db96593c6de29b02282172ab542395a136ac634
vicjune <victor@ileotech.com>
2017-05-24 12:34:02 +0200

    [radio] rename module

---
+++ radio/radio/RadioClient.js
@@ -0,0 +1,107 @@
+let urlModule = require('url');
+let dnsModule = require('dns');
+let netModule = require('net');
+let lame = require('lame');
+let Speaker = require('speaker');
+
+module.exports = RadioClient;
+
+let self;
+function RadioClient () {
+	self = this;
+	this.end = false;
+	this.clientTimeout = null;
+	this.lameDecoder = null;
+	this.speaker = null;
+
+	this.client = new netModule.Socket();
+};
+
+RadioClient.prototype = {
+	connectClient: (url) => {
+		let parsedUrl = urlModule.parse(url);
+		let ipAddress;
+
+		dnsModule.resolve(parsedUrl.hostname, (err, addresses) => {
+			if (addresses) {
+				ipAddress = addresses[0];
+			}
+
+			self.client.connect(parsedUrl.port, ipAddress, () => {
+				self.client.write('Get ' + parsedUrl.path + ' HTTP/1.0\r\n');
+				self.client.write('User-Agent: Mozilla/5.0\r\n');
+				self.client.write('\r\n');
+			});
+		});
+	},
+
+	startStream: (url, fnStart, fnError) => {
+		self.connectClient(url);
+
+		self.lameDecoder = new lame.Decoder();
+		self.speaker = new Speaker();
+		self.client.pipe(self.lameDecoder).pipe(self.speaker);
+
+		let firstPayloadReceived = false;
+		let streamStarted = false;
+
+		self.client.on('data', data => {
+			if (self.clientTimeout) {
+				clearTimeout(self.clientTimeout);
+			}
+			self.clientTimeout = setTimeout(() => {
+				fnError('timeout', self.end);
+
+				self.closeStream(true);
+			}, 10000);
+
+			if (firstPayloadReceived && !self.end && !streamStarted) {
+				fnStart();
+				streamStarted = true;
+			}
+
+			if (!firstPayloadReceived) {
+				firstPayloadReceived = true;
+			}
+		});
+
+		self.client.on('error', err => {
+			self.closeStream(true);
+			fnError(err, true);
+		});
+	},
+
+	stopStream: fnEnd => {
+		self.closeStream(false);
+		let endTimeout = null
+		endTimeout = setTimeout(() => {
+			self.speaker.close();
+			fnEnd();
+		}, 1000);
+
+		self.client.on('data', () => {
+			clearTimeout(endTimeout);
+			endTimeout = setTimeout(() => {
+				self.speaker.close();
+				clearTimeout(self.clientTimeout);
+				fnEnd();
+			}, 1000);
+		});
+	},
+
+	closeStream: closeSpeaker => {
+		self.lameDecoder.unpipe();
+		self.client.destroy();
+		if (self.clientTimeout) {
+			clearTimeout(self.clientTimeout);
+		}
+		if (closeSpeaker) {
+			self.speaker.close();
+		}
+		self.end = true;
+	},
+
+	testUrl: () => {
+
+	}
+}
