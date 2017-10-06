5b1ad196cc6a70fb8ce4057eba2440d7175d7ea3
vicjune <victor.jouin@gmail.com>
2017-10-07 00:10:58 +0200

    test

--- host-device/connection/ConnectionModule.js
+++ host-device/connection/ConnectionModule.js
@@ -50,8 +50,8 @@ module.exports = class ConnectionModule {
 		// Shares memory with `arr`
 		const data = Buffer.from(arr.buffer);
 
-		callback(this.characteristic.RESULT_SUCCESS, data);
-		// callback(this.characteristic.RESULT_SUCCESS, this.toBytes('coucou'));
+		// callback(this.characteristic.RESULT_SUCCESS, data);
+		callback(this.characteristic.RESULT_SUCCESS, this.toBytes('coucou'));
 
 		// wifi.scan((err, networks) => {
 		// 	let result;
@@ -73,7 +73,7 @@ module.exports = class ConnectionModule {
 		for (var i = 0, l = jsonString.length; i < l; i++) {
 			array[i] = jsonString.charCodeAt(i);
 		}
-		return array.buffer;
+		return Buffer.from(array.buffer);
 	}
 
 	fromBytes(buffer) {
