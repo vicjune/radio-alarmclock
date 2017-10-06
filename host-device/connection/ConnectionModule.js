bbf462395c3ff236efaa598f45741f2ae5ceb11c
vicjune <victor.jouin@gmail.com>
2017-10-06 23:19:41 +0200

    test

--- host-device/connection/ConnectionModule.js
+++ host-device/connection/ConnectionModule.js
@@ -5,6 +5,13 @@ let bleno = require('bleno');
 
 module.exports = class ConnectionModule {
 	constructor() {
+		this.characteristic = new bleno.Characteristic({
+			uuid: 'B2FEBA5A-CADB-493C-AD72-34170D046C3B',
+			properties: ['read'],
+			value: null,
+			onReadRequest: this.onReadWifi
+		});
+
 		bleno.on('stateChange', state => {
 			if (state === 'poweredOn') {
 				bleno.startAdvertising('Mouton', ['1720648C-11ED-4847-9F49-86E839B6C9BE']);
@@ -15,13 +22,6 @@ module.exports = class ConnectionModule {
 
 		bleno.on('advertisingStart', error => {
 			if (!error) {
-				this.characteristic = new bleno.Characteristic({
-					uuid: 'B2FEBA5A-CADB-493C-AD72-34170D046C3B',
-					properties: ['read'],
-					value: null,
-					onReadRequest: this.onReadWifi
-				});
-
 				bleno.setServices([
 					new bleno.PrimaryService({
 						uuid: '4F5E0138-2439-4A16-8311-D4F1C500613B',
@@ -39,17 +39,25 @@ module.exports = class ConnectionModule {
 	onReadWifi(offset, callback) {
 		console.log('read');
 
-		wifi.scan((err, networks) => {
-			let data;
-			if (!err) {
-				// let result = this.characteristic.RESULT_SUCCESS;
-				data = new Buffer(0).toString(networks);
-			} else {
-				// let result = this.characteristic.RESULT_SUCCESS;
-				data = new Buffer(0).toString('Error in wifi scan');
-			}
-			callback(null, data);
-		});
+		console.log(this.characteristic);
+
+		result = this.characteristic.RESULT_SUCCESS;
+		data = new Buffer(0).toString('Error in wifi scan');
+
+		callback(null, data);
+
+		// wifi.scan((err, networks) => {
+		// 	let result;
+		// 	let data;
+		// 	if (!err) {
+		// 		result = this.characteristic.RESULT_SUCCESS;
+		// 		data = new Buffer(0).toString(networks);
+		// 	} else {
+		// 		result = this.characteristic.RESULT_SUCCESS;
+		// 		data = new Buffer(0).toString('Error in wifi scan');
+		// 	}
+		// 	callback(null, data);
+		// });
 	}
 }
 
